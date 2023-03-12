d573bea2f57812c9d78c8c383a834fcef2e69364
rxn7 <rotthin_dev@protonmail.com>
2023-03-12 18:20:25 +0100

    Temporarily disabled inventory (which is broken btw) and aligned the tables to the floor grid to please my OCD

--- Assets/VsR/Runtime/Scripts/Player/PlayerInventory.cs
+++ Assets/VsR/Runtime/Scripts/Player/PlayerInventory.cs
@@ -1,23 +1,16 @@
 using UnityEngine;
 
 namespace VsR {
-	[System.Serializable]
-	public struct PlayerInventorySocketData {
-		public Transform transform;
-		[Range(0.0f, 1.0f)] public float heightRatio;
-	}
-
 	[ExecuteAlways]
 	public class PlayerInventory : MonoBehaviour {
 		[SerializeField] private Transform m_head;
-		[SerializeField] private PlayerInventorySocketData[] m_sockets;
+		[SerializeField] private float m_rotationFollowSpeed = 20;
 
 		private void Update() {
 			transform.position = m_head.position;
-			transform.rotation = new Quaternion(transform.rotation.x, m_head.rotation.y, transform.rotation.z, m_head.rotation.w);
 
-			foreach (PlayerInventorySocketData socketData in m_sockets)
-				socketData.transform.position = new Vector3(socketData.transform.position.x, m_head.position.y * socketData.heightRatio, socketData.transform.position.z);
+			Quaternion targetRotation = new Quaternion(transform.rotation.x, m_head.rotation.y, transform.rotation.z, m_head.rotation.w);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, m_rotationFollowSpeed * Time.deltaTime);
 		}
 	}
 }
---
+++ Assets/VsR/Runtime/Scripts/Player/PlayerInventorySocket.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace VsR {
+	[ExecuteAlways]
+	public class PlayerInventorySocket : XRSocketInteractor {
+		[SerializeField] private Transform m_head;
+		[SerializeField] private float m_heightRatio;
+		private XRBaseInteractable m_trackedItem;
+
+		private void Update() {
+			transform.position = new Vector3(transform.position.x, m_head.position.y * m_heightRatio, transform.position.z);
+		}
+
+		private void OnTrackItemSelectExited(SelectExitEventArgs args) {
+		}
+
+		protected override void OnHoverEntered(HoverEnterEventArgs args) {
+			base.OnHoverEntered(args);
+			m_trackedItem = (XRBaseInteractable)args.interactableObject;
+			m_trackedItem.selectExited.AddListener(OnTrackItemSelectExited);
+		}
+
+		protected override void OnHoverExited(HoverExitEventArgs args) {
+			base.OnHoverExited(args);
+			m_trackedItem?.selectExited.RemoveListener(OnTrackItemSelectExited);
+			m_trackedItem = null;
+		}
+
+		public override bool CanSelect(IXRSelectInteractable interactable) {
+			if (!IsSelecting(interactable) && !interactable.interactorsSelecting.Any(i => i is Hand))
+				return false;
+
+			return base.CanSelect(interactable);
+		}
+	}
+}

695e3056ddb2bde5736535800c11f83762d84c3a
rxn7 <rotthin_dev@protonmail.com>
2023-03-19 22:52:32 +0100

    Grabbing with HandInteractionRay

--- Assets/VsR/Runtime/Scripts/Player/Hand/HandInteractionRay.cs
+++ Assets/VsR/Runtime/Scripts/Player/Hand/HandInteractionRay.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace VsR {
 	[RequireComponent(typeof(LineRenderer))]
 	public class HandInteractionRay : MonoBehaviour {
 		[SerializeField] private Hand m_hand;
-		[SerializeField] private float m_maxDistance = 50;
+		[SerializeField] private float m_maxDistance = 5;
 		[SerializeField] private LayerMask m_rayLayerMask;
+
 		private InputAction m_enableRaycastAction;
 		private LineRenderer m_lineRenderer;
 		private Ray m_ray;
+		private XRBaseInteractable m_hoveringInteractable = null;
+
+		public bool IsHovering => m_hoveringInteractable != null;
 
 		private void Awake() {
 			m_lineRenderer = GetComponent<LineRenderer>();
@@ -21,8 +26,9 @@ namespace VsR {
 		}
 
 		private void LateUpdate() {
-			if (!m_enableRaycastAction.IsPressed()) {
+			if (!m_enableRaycastAction.IsPressed() || m_hand.interactablesSelected.Count > 0) {
 				m_lineRenderer.enabled = false;
+				m_hoveringInteractable = null;
 				return;
 			}
 
@@ -32,15 +38,43 @@ namespace VsR {
 			m_ray.origin = transform.position;
 			m_ray.direction = transform.forward;
 
-			Vector3 rayEndPoint;
+			Color color = Color.clear;
+			Vector3 rayEndPoint = Vector3.zero;
+
 			if (Physics.Raycast(m_ray, out RaycastHit hit, m_maxDistance, m_rayLayerMask)) {
 				rayEndPoint = hit.point;
+				color = Color.green;
+
+				if (hit.transform.TryGetComponent<XRBaseInteractable>(out XRBaseInteractable interactable))
+					m_hoveringInteractable = interactable;
+				else
+					m_hoveringInteractable = null;
 			} else {
-				rayEndPoint = transform.position + m_ray.direction * m_maxDistance;
+				m_hoveringInteractable = null;
 			}
+
+			if (!IsHovering) {
+				m_hoveringInteractable = null;
+				rayEndPoint = transform.position + m_ray.direction * m_maxDistance;
+				color = Color.red;
+			}
+
+			m_lineRenderer.material.color = color;
 
 			m_lineRenderer.SetPosition(0, m_hand.attachTransform.position);
 			m_lineRenderer.SetPosition(1, rayEndPoint);
+
+			if (m_hand.GrabAction.WasPressedThisFrame()) {
+				OnGrab();
+			}
+		}
+
+		private void OnGrab() {
+			if (!IsHovering || m_hand.interactablesSelected.Count > 0)
+				return;
+
+			m_hand.interactionManager.SelectEnter(m_hand, (IXRSelectInteractable)m_hoveringInteractable);
+			m_hoveringInteractable = null;
 		}
 	}
 }

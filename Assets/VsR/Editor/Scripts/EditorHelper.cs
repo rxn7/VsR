66a175d03880a1314c53fede4a4aaf0a5ed48691
rxn7 <rotthin_dev@protonmail.com>
2023-03-24 15:15:26 +0100

    Fixed potential issues with assets being overwritten

--- Assets/VsR/Editor/Scripts/EditorHelper.cs
+++ Assets/VsR/Editor/Scripts/EditorHelper.cs
@@ -6,6 +6,10 @@ namespace VsR.Editors {
 	public static class EditorHelper {
 		public static void CreateAssetSafe(Object asset, string path) {
 			Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+			if (File.Exists(path))
+				throw new System.Exception($"Cannot create asset on path: {path}, because file with this path already exists!");
+
 			AssetDatabase.CreateAsset(asset, path);
 			AssetDatabase.Refresh();
 		}

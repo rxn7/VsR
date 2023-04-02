a3c9d29d3f1143dbb09b9aa8fd3a614133411c35
rxn7 <rotthin_dev@protonmail.com>
2023-04-02 02:17:13 +0200

    Apply force feedback to grip hand on WeaponBoltCatch slap

--- Assets/VsR/Runtime/Scripts/Weapon System/Weapon Parts/WeaponBoltCatch.cs
+++ Assets/VsR/Runtime/Scripts/Weapon System/Weapon Parts/WeaponBoltCatch.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 namespace VsR {
 	public class WeaponBoltCatch : MonoBehaviour, IWeaponPart {
 		[field: SerializeField] public Weapon Weapon { get; set; }
-		[SerializeField] private AudioClip m_boltReleaseClip;
+		[SerializeField] private AudioClip m_sound;
+		[SerializeField] private HapticFeedback m_feedback = new HapticFeedback(0.3f, 0.1f);
 		[SerializeField] public WeaponBolt m_bolt;
+		[SerializeField] private WeaponMagazineSlot m_magSlot;
 
 		private void Awake() {
 			IWeaponPart.Validate(this);
 		}
 
 		protected void OnTriggerEnter(Collider collider) {
-			if (!collider.gameObject.TryGetComponent<Hand>(out Hand hand) || !m_bolt.IsOpen)
+			if (!collider.gameObject.TryGetComponent<Hand>(out Hand hand) || !m_bolt.IsOpen || !Weapon.GripHand)
 				return;
 
 			if (hand.interactablesSelected.Contains(Weapon))
@@ -21,10 +23,12 @@ namespace VsR {
 		}
 
 		public void CloseBolt() {
+			Weapon.GripHand?.ApplyHapticFeedback(m_feedback);
+
 			m_bolt.IsOpen = false;
 			Weapon.TryToCock();
 
-			SoundPoolManager.Instance.PlaySound(m_boltReleaseClip, transform.position, Random.Range(0.9f, 1.1f));
+			SoundPoolManager.Instance.PlaySound(m_sound, transform.position, Random.Range(0.9f, 1.1f));
 		}
 	}
 }
